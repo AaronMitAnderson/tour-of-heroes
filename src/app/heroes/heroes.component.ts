ad219c897dfed42beb5fe9e5291dc3a9f71a179a
AaronMitAnderson <AaronMitAnderson@gmail.com>
2021-05-20 11:19:44 -0400

    Add Services
    Made access to mock data asynchronous through the HeroService class
    Created MessageService class for display of communication at bottom of screen

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../services/hero.service';
+import { MessageService } from '../services/message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +10,28 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes = HEROES;
   selectedHero?: Hero;
+  heroes: Hero[] = [];
+
 
   // hero: Hero = {
   //   id: 1, 
   //   name: 'Windstorm'
   // };
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void{
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected Hero id = ${hero.id}`);
+  }
+
+  getHeroes(): void{
+    this.heroService.getHeros().subscribe(heroes => this.heroes = heroes)
   }
 
 }
